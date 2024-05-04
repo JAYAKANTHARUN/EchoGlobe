350b641ceddb0498b26ee4d4748ce26219194ef4
Jayakanth Arun <jayakantharun03@gmail.com>
2024-05-04 18:54:54 +0530

    Refined Project Structure

---
+++ client/src/components/Translator.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-3xl font-poppins">
+        Home
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Translator from './components/Translator.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/index.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Translator },
+  ]
+})
+
+createApp(App).use(router).mount('#app')
